bbced3b061af97524f66552700d8ca97c2c4e7bd
leirdas <adrielsoh01@gmail.com>
2024-02-04 18:29:26 +0800

    Setup firebase authentication

---
+++ hack4good/src/pages/LoginPage.tsx
@@ -0,0 +1,74 @@
+import React, { useContext, useRef } from "react";
+import { Button, Col, Container, Form, Navbar, Row } from "react-bootstrap";
+import { AuthContext } from "./context/AuthContext";
+import { auth } from "../firebaseSetup";
+
+function LoginPage({ user }) {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const createAccount = async () => {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signIn = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    await auth.signOut();
+  };
+
+  return (
+    <div>
+      <Navbar className="justify-content-between" bg="dark" variant="dark">
+        <Navbar.Brand>Firebase Authentication</Navbar.Brand>
+        {user && <Button onClick={signOut}>Sign Out</Button>}
+      </Navbar>
+      <Container style={{ maxWidth: "500px" }} fluid>
+        <Form className="mt-4">
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control ref={emailRef} type="email" placeholder="email" />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="password"
+            />
+          </Form.Group>
+          <Row>
+            <Col xs={6}>
+              <Button onClick={createAccount} type="button">
+                Sign Up
+              </Button>
+            </Col>
+            <Col xs={6}>
+              <Button onClick={signIn} type="button" variant="secondary">
+                Sign In
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
+}
+
+export default LoginPage;
