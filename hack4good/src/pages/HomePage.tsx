5a3ed87bb963baab5e69c61f7848f95664d569eb
leirdas <adrielsoh01@gmail.com>
2024-02-08 14:00:32 +0800

    Minor changes to home page

--- hack4good/src/pages/HomePage.tsx
+++ hack4good/src/pages/HomePage.tsx
@@ -66,39 +66,39 @@ function HomePage({ user }) {
         </Container>
       </Navbar> */}
       <div className="container flex flex-wrap justify-between">
-      {opportunities.map((opportunity) => (
-        <Card
-          className="w-[300px] m-4 h-[350px]"
-         // style={{ backgroundColor: "#FAF9F6" }}
-        >
-          <CardHeader>
-            <CardTitle>{opportunity.event}</CardTitle>
-            <CardDescription>{opportunity.date}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Location</Label>
-                  <CardDescription>{opportunity.location}</CardDescription>
+        {opportunities.map((opportunity) => (
+          <Card
+            className="w-[300px] m-4 h-[350px]"
+            // style={{ backgroundColor: "#FAF9F6" }}
+          >
+            <CardHeader>
+              <CardTitle>{opportunity.event}</CardTitle>
+              <CardDescription>{opportunity.date}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Organisation:</Label>
+                    <CardDescription>{opportunity.location}</CardDescription>
+                  </div>
+                  <div className="flex-wrap items-center">
+                    <Label htmlFor="framework">Target Group(s):</Label>
+                    {opportunity.target.map((target) => (
+                      <Badge className="w-[100px] m-1" variant="secondary">
+                        {target}
+                      </Badge>
+                    ))}
+                  </div>
                 </div>
-                <div className="flex-wrap items-center">
-                  <Label htmlFor="framework">Target Group(s):</Label>
-                  {opportunity.target.map((target) => (
-                    <Badge className="w-[100px] m-1" variant="secondary">
-                      {target}
-                    </Badge>
-                  ))}
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            {/* <Button variant="outline">Cancel</Button> */}
-            <Button>Register</Button>
-          </CardFooter>
-        </Card>
-      ))}
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              {/* <Button variant="outline">Cancel</Button> */}
+              <Button>Register</Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   );
